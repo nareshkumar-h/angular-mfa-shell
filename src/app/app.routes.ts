7e7964ddf6767361c4995c36c3a6a0178a87148f
nareshkumar-h <nareshkumarh@live.com>
2024-11-27 12:35:26 +0530

    updated configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tasks',
+    loadComponent: () => loadRemoteModule('taskapp', './TaskListComponent').then((m) => m.TaskListComponent),
+  },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+];
